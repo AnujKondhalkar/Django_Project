a7f720ebce2902f8079fa8ec59336098fa3d5186
Anuj Kondhalkar <anujkondhalkar@outlook.com>
2022-07-10 23:11:22 +0530

    Added templates

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
-# Create your views here.
+posts = [
+    {
+        'author': 'John Cena',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2022'
+    },
+    {
+        'author': 'Iron Man',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2022'
+    }
+]
 
 
 def home(request):
-    return HttpResponse("<h1>Blog Home</h1>")
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return HttpResponse("<h1>Blog About</h1>")
+    return render(request, 'blog/about.html')
