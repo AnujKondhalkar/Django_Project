43ea59220561de4717521dc1104f5f0a4b92911f
Anuj Kondhalkar <anujkondhalkar@outlook.com>
2022-07-15 00:35:57 +0530

    Registered posts to admin.py And removed dummy data
    All useres registered , able to change the data content through django gui.
    Worked with data using django sqlite db

--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,10 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'John Cena',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'April 20, 2022'
-    },
-    {
-        'author': 'Iron Man',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'April 21, 2022'
-    }
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
