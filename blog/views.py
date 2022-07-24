9a7a2389668639f7fd33d36e3465b280448c1f61
Anuj Kondhalkar <anujkondhalkar@outlook.com>
2022-07-24 18:04:12 +0530

    Added deatil view of post PostListView, PostDetailView Routes to see post by ID

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 
@@ -7,6 +8,21 @@ def home(request):
         'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
+
+
+class PostListView(ListView):
+    # django convenstions
+    # This 'class (Post)'' will be imported from .model of user app
+    model = Post
+    # django naming --> convenstions <app>/<model>_<viewtype>.html >>> blog/home.html
+    template_name = 'blog/home.html'
+    # in html home.html page this variable 'posts will be delivered'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']        # '-' for newest post first
+
+
+class PostDetailView(DetailView):
+    model = Post
 
 
 def about(request):
--- users/views.py
+++ users/views.py
@@ -21,26 +21,32 @@ def register(request):
 
 @login_required
 def profile(request):
-    if request.method == 'POST':
-        u_form = UserUpdateForm(request.POST, instance=request.user)
-        p_form = ProfileUpdateForm(
-            request.POST,
-            request.FILES,
-            instance=request.user.profile)
-        if u_form.is_valid() and p_form.is_valid():
-            u_form.save()
-            p_form.save()
-            messages.success(
-                request, f'Your account has been updated!')
-            return redirect('profile')
+    if request.user.is_authenticated:
+        if request.method == 'POST':
+            # u_form and p_form these forms are rendered from users/forms.py
+            u_form = UserUpdateForm(request.POST, instance=request.user)
+            p_form = ProfileUpdateForm(
+                request.POST,
+                request.FILES,
+                instance=request.user.profile)
+            if u_form.is_valid() and p_form.is_valid():
+                u_form.save()
+                p_form.save()
+                messages.success(
+                    request, f'Your account has been updated!')
+                return redirect('profile')
 
-    else:
-        u_form = UserUpdateForm(instance=request.user)
-        p_form = ProfileUpdateForm(instance=request.user.profile)
+        else:
+            u_form = UserUpdateForm(instance=request.user)
+            p_form = ProfileUpdateForm(instance=request.user.profile)
 
-    context = {
-        'u_form': u_form,
-        'p_form': p_form
-    }
+        context = {
+            'u_form': u_form,
+            'p_form': p_form
+        }
+    else:                   # if not request.user.is_authenticated
+        messages.info(
+            request, f'You need to log in first! to access this page.')
+        return redirect('login')
 
     return render(request, 'users/profile.html', context)
