81dc205d3bc1d7b1b6c13a7e9fc8c0149151141d
Anuj Kondhalkar <anujkondhalkar@outlook.com>
2022-07-25 22:34:21 +0530

    Added user specific post view with url in html templates

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.models import User
 from django.views.generic import (
     ListView,
     DetailView,
@@ -28,6 +29,21 @@ class PostListView(ListView):
     context_object_name = 'posts'
     ordering = ['-date_posted']        # '-' for newest post first
     paginate_by = 5
+
+
+class UserPostListView(ListView):
+    # django convenstions
+    # This 'class (Post)'' will be imported from .model of user app
+    model = Post
+    # django naming --> convenstions <app>/<model>_<viewtype>.html >>> blog/home.html
+    template_name = 'blog/user_posts.html'
+    # in html home.html page this variable 'posts will be delivered'
+    context_object_name = 'posts'
+    paginate_by = 5
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-date_posted')
 
 
 class PostDetailView(DetailView):
